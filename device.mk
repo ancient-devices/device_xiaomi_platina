8fd1beaf014f9c57ebd1ceb4637395b807b671e8
ricksanchez777 <henripaz1998@gmail.com>
2020-03-30 03:43:19 +0000

    platina: Prebuilt: Add ANXCamera and drop Snap

--- device.mk
+++ device.mk
@@ -134,7 +134,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.5 \
     vendor.qti.hardware.camera.device@1.0 \
-    Snap
+    ANXCamera
 
 # Codec2 modules
 PRODUCT_PACKAGES += \
